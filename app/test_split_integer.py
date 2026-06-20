fea6a287813800e4a2c135bf296b763e6689428f
Vitalii <stalchenkovitaliy@gmail.com>
2026-06-20 12:22:55 +0400

    Solved task with tests

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -2,20 +2,49 @@ from app.split_integer import split_integer
 
 
 def test_sum_of_the_parts_should_be_equal_to_value() -> None:
-    pass
+    value = 32
+    number_of_parts = 6
+    result = split_integer(value, number_of_parts)
+    assert sum(result) == value
 
 
-def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
-    pass
+def test_should_return_exact_number_of_parts() -> None:
+    value = 6
+    number_of_parts = 2
+    result = split_integer(value, number_of_parts)
+    assert result == [3, 3]
 
 
 def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
-    pass
+    value = 8
+    number_of_parts = 1
+    result = split_integer(value, number_of_parts)
+    assert result == [8]
 
 
 def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
-    pass
+    value = 17
+    number_of_parts = 4
+    result = split_integer(value, number_of_parts)
+    assert result == sorted(result)
 
 
 def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
-    pass
+    value = 2
+    number_of_parts = 3
+    result = split_integer(value, number_of_parts)
+    assert result == [0, 1, 1]
+
+
+def test_len_of_the_parts_should_be_equal_to_value() -> None:
+    value = 32
+    number_of_parts = 6
+    result = split_integer(value, number_of_parts)
+    assert len(result) == number_of_parts
+
+
+def test_difference_between_the_largest_and_the_smallest_part_is_one() -> None:
+    value = 32
+    number_of_parts = 6
+    result = split_integer(value, number_of_parts)
+    assert max(result) - min(result) <= 1
