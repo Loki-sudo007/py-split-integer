2bba585c40d37c59e08b46851ac87ea663f9a1f6
Vitalii <stalchenkovitaliy@gmail.com>
2026-06-20 13:05:39 +0400

    Fix problems

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -22,7 +22,7 @@ def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
 def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
     value = 17
     number_of_parts = 4
-    assert split_integer(value, number_of_parts) == sorted(split_integer(value, number_of_parts))
+    assert split_integer(value, number_of_parts) == [4, 4, 4, 5]
 
 
 def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
@@ -37,11 +37,11 @@ def test_len_of_the_parts_should_be_equal_to_number_of_parts() -> None:
     assert len(split_integer(value, number_of_parts)) == number_of_parts
 
 
-def test_difference_between_the_largest_and_the_smallest_part_is_one() -> None:
+def test_max_min_difference_should_not_exceed_one() -> None:
     value = 32
     number_of_parts = 6
     assert (
-            max(split_integer(value, number_of_parts))
-            - min(split_integer(value, number_of_parts))
-            <= 1
+        max(split_integer(value, number_of_parts))
+        - min(split_integer(value, number_of_parts))
+        <= 1
     )
